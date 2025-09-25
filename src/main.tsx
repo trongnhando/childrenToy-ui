de7c6207e79a3a45ad01c22c8618f6d995eebd56
dtnhan <dotrongnhan15102000@gmail.com>
2025-09-25 19:54:27 +0700

    local config

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
 import './index.css';
@@ -9,13 +9,13 @@ import { ContextProvider } from './context/SumProductContext.tsx';
 import store from './ducks/store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Provider store={store}>
-        <ContextProvider>
-          <App />
-        </ContextProvider>
-      </Provider>
-    </HashRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </Provider>
+  </BrowserRouter>
+  // </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://trongnhando.github.io/childrenToy-ui/',
   plugins: [react()],
   server: {
     port: 3000,
